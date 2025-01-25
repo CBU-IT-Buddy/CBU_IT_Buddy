152094bffb09cc8501cdecdc0b30e71129537e20
D'Artagnan Calitz <90787700+DArtagnanCalitz@users.noreply.github.com>
2025-01-24 17:29:00 -0800

    Refactored code

---
+++ cbu_it_buddy/lib/screens/chat_page.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'chat_bubbles.dart';
+import 'helpers/chatbot_helper.dart';
+import 'services/bible_service.dart';
+import 'widgets/option_card.dart';
+
+class ChatPage extends StatefulWidget {
+  final String query;
+
+  const ChatPage({Key? key, required this.query}) : super(key: key);
+
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final ChatbotHelper _chatbotHelper = ChatbotHelper();
+  final List<Map<String, dynamic>> _chatMessages = [];
+  final TextEditingController _textController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+  bool _hasSubmittedMessage = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchBibleVerse(); // Fetch the Bible verse when the page loads
+  }
+
+  Future<void> _fetchBibleVerse() async {
+    String verse = await BibleService().fetchBibleVerse();
+    setState(() {
+      _chatMessages.add({"message": verse, "isUserMessage": false});
+    });
+  }
+
+  void _handleSubmitMessage() async {
+    if (_textController.text.isNotEmpty) {
+      final String userMessage = _textController.text;
+
+      setState(() {
+        _chatMessages.add({"message": userMessage, "isUserMessage": true});
+        _hasSubmittedMessage = true;
+      });
+
+      String botResponse = await _chatbotHelper.getAnswer(userMessage);
+
+      setState(() {
+        _chatMessages.add({"message": botResponse, "isUserMessage": false});
+        _textController.clear();
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Chat with IT Buddy"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              reverse: true,
+              itemCount: _chatMessages.length,
+              itemBuilder: (context, index) {
+                final messageData =
+                    _chatMessages[_chatMessages.length - 1 - index];
+                return ChatBubble(
+                  message: messageData['message'],
+                  isUserMessage: messageData['isUserMessage'],
+                );
+              },
+            ),
+          ),
+          if (!_hasSubmittedMessage)
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Row(
+                  children: [
+                    const SizedBox(width: 16.0),
+                    OptionCard(
+                      title: "Reset Password",
+                      subtitle: "All Accounts",
+                      onTap: () => _submitQuickMessage("Reset Password"),
+                    ),
+                    OptionCard(
+                      title: "Reset MFA",
+                      subtitle: "Microsoft Authenticator, phone number",
+                      onTap: () => _submitQuickMessage("Reset MFA"),
+                    ),
+                    OptionCard(
+                      title: "WiFi - Problems",
+                      subtitle: "CBU-SECURE, CBU...",
+                      onTap: () => _submitQuickMessage("WiFi - Problems"),
+                    ),
+                    const SizedBox(width: 16.0),
+                  ],
+                ),
+              ),
+            ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    focusNode: _focusNode,
+                    controller: _textController,
+                    decoration: InputDecoration(
+                      hintText: "Type your message...",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 16.0),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                IconButton(
+                  icon: const Icon(Icons.send,
+                      color: Color.fromARGB(255, 11, 54, 90)),
+                  onPressed: _handleSubmitMessage,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _submitQuickMessage(String message) {
+    setState(() {
+      _textController.text = message;
+    });
+    _handleSubmitMessage();
+  }
+}
